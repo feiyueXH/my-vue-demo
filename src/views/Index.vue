a3719682602cb00ea4fd3febbb684e1d18d968bb
飞岳星痕 <1203986130@qq.com>
2020-03-31 00:20:09 +0800

    20200331

---
+++ src/views/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+
+  computed:{
+    msg:function(){
+      return "当前状态值:"+this.$store.state.count;
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>欢迎来到登录页面</div>
+  <div>
+    欢迎来到登录页面
+    <button @click="addCount">叠加值</button>  
+    <button @click="linkToIndex">进入主页</button>  
+  </div>
 </template>
 <script>
 export default {
   data(){
     return {
       
+    }
+  },
+
+  methods:{
+    linkToIndex(){
+      this.$router.push({path:'/index'});
+    },
+
+    addCount(){
+      this.$store.commit('changeCount');
     }
   }
 }
