14a5f59659fed457e03bc31120fd4206400b5fe7
feiyueXH <feiyueXH@1203986130@qq.com>
2020-04-20 20:12:48 +0800

    回滚代码,回到可以打包显示页面

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,28 +1,29 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const baseConfig = require('./webpack.base');
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
 
 const devConfig = {
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
-  
+
   devServer: {
-    host: 'localhost',   //服务器的ip地址
-    port: "1573",  //端口
-    open: true,  //自动打开页面
+    host: 'localhost', //服务器的ip地址
+    port: '1573', //端口
+    open: true, //自动打开页面
     hot: true, //开启热更新
-    proxy: {//启用代理
+    proxy: {
+      //启用代理
       '/api/v1/**': {
         target: 'https://cnodejs.org/',
         secure: false,
-        changeOrigin: false,
-      },
+        changeOrigin: false
+      }
     }
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin()    //引入热更新插件
-  ],  
+    new webpack.HotModuleReplacementPlugin() //引入热更新插件
+  ]
 }
 
-module.exports = merge(baseConfig, devConfig);
+module.exports = merge(baseConfig, devConfig)
--- src/router/index.js
+++ src/router/index.js
@@ -19,64 +19,38 @@ Router.prototype.replace = function replace(location) {
 
 Vue.use(Router)
 
-// /**
-//  * 加载view组件
-//  * @param {*} name 别名
-//  * @param {*} view 视图名
-//  * @param {*} version 版本号
-//  */
-function loadView(name, path) {
-  // console.log('>>>>>>>>>>>>>>>>view:', view)
-  // return r => require.ensure([], () => r(require(`@/views/${view}/${view}_${version}`)), `${view}_${version}`)
-  return () => import(/* webpackChunkName: "[request]" */ path)
-  // return () => import(`@/views/${view}/${view}_${version}.vue`)
-}
-
-const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    component: () =>
-      import(/* webpackChunkName: "[request]" */ '@/views/Login/Login_1.0.0')
-  }
-]
-
 export default new Router({
-  mode: 'hash',
-  routes: routes
-  // routes: [
-  //   {
-  //     path: '/login',
-  //     name: 'login',
-  //     // component: (r) =>
-  //     //   require.ensure(
-  //     //     [],
-  //     //     () => r(require('@/views/Login/Login_1.0.0')),
-  //     //     'login_1.0.0'
-  //     //   )
-  //     component: loadView('login', '@/views/Login/Login_1.0.0.vue')
-  //   }
-  //   // {
-  //   //   path: '/home',
-  //   //   name: 'home',
-  //   //   component: (r) =>
-  //   //     require.ensure(
-  //   //       [],
-  //   //       () => r(require('@/views/Home/Home_1.0.0')),
-  //   //       'home_1.0.0'
-  //   //     )
-  //   //   // component: loadView('home','Home','1.0.0')
-  //   // },
-  //   // {
-  //   //   path: '/about',
-  //   //   name: 'about',
-  //   //   component: (r) =>
-  //   //     require.ensure(
-  //   //       [],
-  //   //       () => r(require('@/views/About/About_1.0.0')),
-  //   //       'about_1.0.0'
-  //   //     )
-  //   //   // component: loadView('index','Index','1.0.0')
-  //   // }
-  // ]
+  mode: 'history',
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: (r) =>
+        require.ensure(
+          [],
+          () => r(require('@/views/Login/Login_1.0.0')),
+          'login_1.0.0'
+        )
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: (r) =>
+        require.ensure(
+          [],
+          () => r(require('@/views/Home/Home_1.0.0')),
+          'home_1.0.0'
+        )
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: (r) =>
+        require.ensure(
+          [],
+          () => r(require('@/views/About/About_1.0.0')),
+          'about_1.0.0'
+        )
+    }
+  ]
 })
